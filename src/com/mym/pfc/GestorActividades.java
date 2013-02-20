7c16c69bfb694694d5682c152da279d4e3456897
mmoreno2 <mmoreno2@DESA3320.tse.ads.t-systems.es>
2013-02-20 16:59:44 +0100

    Modificacion de Launcher intentando agregar intent

--- src/com/mym/pfc/GestorActividades.java
+++ src/com/mym/pfc/GestorActividades.java
@@ -1,15 +1,39 @@
 package com.mym.pfc;
 
+import com.mym.pfc.actividades.Balones;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class GestorActividades extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_gestor_actividades);
+		
+		
+		
+		//super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_gestor_actividades);
+		try {
+			
+			Intent intent = new Intent();
+	        intent.setClass(this, Balones.class);
+	        this.startActivity(intent);
+			/*Bundle myData = new Bundle();
+			myData.putString("myString1", "Hello Android");
+			myData.putDouble("myDouble1", 3.141592);
+			int[] myLittleArray = { 1, 2, 3 };
+			myData.putIntArray("myIntArray1", myLittleArray);
+
+			myIntentA1A2.putExtras(myData);
+
+			startActivityForResult(myIntentA1A2,IPC_ID);*/
+		} catch (Exception e) {
+			
+		}
+		
 	}
 
 	@Override

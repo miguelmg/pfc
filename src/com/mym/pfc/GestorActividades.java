a353ce35d33a04aeb51ba7a6821614605b848fd8
miguelmg <miguelmorgom@gmail.com>
2013-02-21 00:01:41 +0100

    Modificada clase GestorActividades(ahora llama a balones) Añadida balones a AndroidManifest Queda probar en un dispositivo con gpu activada....

--- src/com/mym/pfc/GestorActividades.java
+++ src/com/mym/pfc/GestorActividades.java
@@ -5,7 +5,11 @@ import com.mym.pfc.actividades.Balones;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class GestorActividades extends Activity {
 
@@ -13,29 +17,37 @@ public class GestorActividades extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gestor_actividades);
 		
-		//super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_gestor_actividades);
-		try {
-			
-			Intent intent = new Intent();
-	        intent.setClass(this, Balones.class);
-	        this.startActivity(intent);
-			/*Bundle myData = new Bundle();
-			myData.putString("myString1", "Hello Android");
-			myData.putDouble("myDouble1", 3.141592);
-			int[] myLittleArray = { 1, 2, 3 };
-			myData.putIntArray("myIntArray1", myLittleArray);
-
-			myIntentA1A2.putExtras(myData);
-
-			startActivityForResult(myIntentA1A2,IPC_ID);*/
-		} catch (Exception e) {
-			
-		}
+		
+		Button buton = (Button)findViewById(R.id.buton);
+		 
+        //Implementamos el evento “click” del botón
+        buton.setOnClickListener(new OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                  //Creamos el Intent
+                  Intent intent = new Intent(GestorActividades.this, Balones.class);
+ 
+                  //Creamos la información a pasar entre actividades
+                  //Bundle b = new Bundle();
+                  //b.putString("NOMBRE", txtNombre.getText().toString());
+ 
+                  //Añadimos la información al intent
+                  //intent.putExtras(b);
+ 
+                  //Iniciamos la nueva actividad
+                  startActivity(intent);
+             }
+        });
 		
 	}
 
+	public void onClick(View view){
+        startActivity(new Intent(this, Balones.class));
+    }
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
